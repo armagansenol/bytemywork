cc6c0f2804a18e5772d7b159dfe04708cfc04f1a
armagansenol <armagansnl@gmail.com>
2025-01-19 13:59:08 +0300

    refactor: enhance ProjectCard component layout and styling
    - Updated the grid layout and spacing in the ProjectCard component for improved responsiveness and visual consistency.
    - Changed the title and tag styles for better readability and alignment with design standards.
    - Adjusted the description font weight for a more polished appearance.
    - Modified the image container to utilize aspect ratio classes for better image handling.

--- frontend/components/shared/project-card/index.tsx
+++ frontend/components/shared/project-card/index.tsx
@@ -13,18 +13,20 @@ interface ProjectCardProps {
 
 export function ProjectCard({ title, tags, description, image, slug }: ProjectCardProps) {
   return (
-    <Link className="cursor-pointer grid md:grid-cols-2 gap-8 items-start" href={`/work/${slug}`}>
-      <div className="space-y-6">
-        <h2 className="text-2xl font-bold">{title}</h2>
-        <div className="flex gap-4">
-          {tags.map((tag, tagIndex) => (
-            <span key={tagIndex} className="text-sm text-gray-400">
-              {tag}
-            </span>
-          ))}
+    <Link className="cursor-pointer grid grid-cols-24 gap-8 items-start" href={`/work/${slug}`}>
+      <div className="col-span-12 space-y-10">
+        <div className="space-y-2">
+          <h2 className="text-base font-semibold">{title}</h2>
+          <div className="flex gap-4">
+            {tags.map((tag, tagIndex) => (
+              <span key={tagIndex} className="text-sm text-namara-grey">
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
-        <p className="text-gray-400 max-w-md">{description}</p>
-        <button className="cursor-pointer flex items-center gap-2">
+        <p className="font-light max-w-md">{description}</p>
+        <button className="cursor-pointer font-semibold flex items-center gap-2">
           <span className="block"> [ </span>
           <span className="block">
             <ScrambleHover
@@ -39,7 +41,7 @@ export function ProjectCard({ title, tags, description, image, slug }: ProjectCa
           <span className="block"> ] </span>
         </button>
       </div>
-      <div className="relative h-[400px]">
+      <div className="col-span-12 aspect-w-4 aspect-h-3">
         <Img className="object-cover rounded-lg" src={image} alt={title} width={500} height={500} />
       </div>
     </Link>
