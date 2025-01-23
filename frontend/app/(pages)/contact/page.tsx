07e79c1129fe107640b8e35f182f7ac36d24151d
armagansenol <armagansnl@gmail.com>
2025-01-23 19:31:11 +0300

    feat: add contact page styling and layout adjustments
    - Introduced a new CSS module for the contact page to manage layout and styling.
    - Updated the Wrapper component in the contact page to apply the new styles, ensuring the contact section occupies the full viewport height minus the header height.

--- frontend/app/(pages)/contact/page.tsx
+++ frontend/app/(pages)/contact/page.tsx
@@ -1,9 +1,11 @@
+import s from "./contact.module.css"
+
 import { ContactForm } from "@/components/shared/form-contact"
 import { Wrapper } from "@/components/wrapper"
 
 export default async function Page() {
   return (
-    <Wrapper theme="dark" headerVariant="withLogo">
+    <Wrapper className={s.contact} theme="dark" headerVariant="withLogo">
       <section className="container-section grid grid-cols-24 gap-0 md:gap-8 py-12 md:py-12 space-y-10 md:space-y-0">
         <div className="col-span-24 md:col-span-12 space-y-4">
           <h1 className="text-3xl font-bold">DROP A LINE</h1>
